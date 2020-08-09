52089c9b16f37acc0a8edfdf6568f282b03cc929
staveesh <sharmataveesh@yahoo.in>
2020-08-09 14:23:34 +0530

    Changed homepage

---
+++ src/Components/Testimonials.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ParticlesBg  from "particles-bg";
+
+class Testimonials extends Component {
+  render() {
+
+    if(this.props.data){
+      var testimonials = this.props.data.testimonials.map(function(testimonials){
+        return  <li key={testimonials.user}>
+            <blockquote>
+               <img className="testimonial-pic" src={"images/"+testimonials.image}/>
+               <p>{testimonials.text}</p>
+               <cite>{testimonials.user}</cite>
+            </blockquote>
+         </li>
+      })
+    }
+
+    return (
+      <section id="testimonials">
+      <div className="text-container">
+         <div className="row">
+
+            <div className="two columns header-col">
+               <h1><span>Client Testimonials</span></h1>
+            </div>
+
+            <div className="ten columns flex-container">
+                  <ul className="slides">
+                      {testimonials}
+                  </ul>
+               </div>
+            </div>
+         </div>
+   </section>
+    );
+  }
+}
+
+export default Testimonials;
